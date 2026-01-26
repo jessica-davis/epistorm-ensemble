3722b2430fe7107da7de5194fd629f5229beded6
clarabay <98752502+clarabay@users.noreply.github.com>
2026-01-26 15:34:05 -0500

    Update epistorm-ensemble-dashboard.py

--- epistorm-ensemble-dashboard.py
+++ epistorm-ensemble-dashboard.py
@@ -238,6 +238,9 @@ def create_ensemble_forecasts(forecast_data):
     ensemble1 = pd.read_csv('./data/quantile_ensemble.csv')
     categorical_ensemble = pd.read_csv('./data/categorical_ensemble.csv')
 
+    ensemble1['model'] = 'Median Epistorm Ensemble'
+    categorical_ensemble['model'] = 'Median Epistorm Ensemble'
+    
     return pd.concat([ensemble1, categorical_ensemble], ignore_index=True)
 
 def hex_to_rgba(hex_color, alpha):
