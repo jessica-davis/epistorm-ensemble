e165194167cb931376962bc1e7a1d272baa0fe23
clarabay <98752502+clarabay@users.noreply.github.com>
2026-01-27 13:39:32 -0500

    Update epistorm-ensemble-dashboard.py

--- epistorm-ensemble-dashboard.py
+++ epistorm-ensemble-dashboard.py
@@ -230,16 +230,16 @@ def load_coverage_data():
         return None
 
 def create_ensemble_forecasts(forecast_data):
-    ensemble1 = create_ensemble_method1(forecast_data)
+   # ensemble1 = create_ensemble_method1(forecast_data)
+   # ensemble1['model'] = 'Median Epistorm Ensemble'
+   # categorical_ensemble = create_categorical_ensemble_quantile(ensemble1[ensemble1.horizon>=0])
+   # categorical_ensemble['model'] = 'Median Epistorm Ensemble'
+
+    ensemble1 = pd.read_parquet('./data/quantile_ensemble.pq')
+    categorical_ensemble = pd.read_parquet('./data/categorical_ensemble.pq')
+
     ensemble1['model'] = 'Median Epistorm Ensemble'
-    categorical_ensemble = create_categorical_ensemble_quantile(ensemble1[ensemble1.horizon>=0])
     categorical_ensemble['model'] = 'Median Epistorm Ensemble'
-
-   # ensemble1 = pd.read_csv('./data/quantile_ensemble.csv')
-   # categorical_ensemble = pd.read_csv('./data/categorical_ensemble.csv')
-
-    #ensemble1['model'] = 'Median Epistorm Ensemble'
-    #categorical_ensemble['model'] = 'Median Epistorm Ensemble'
 
     return pd.concat([ensemble1, categorical_ensemble], ignore_index=True)
 
