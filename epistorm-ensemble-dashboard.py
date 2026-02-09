9c6adec87c2b529945e16a3c2bc8adbd1ebad5e0
clarabay <98752502+clarabay@users.noreply.github.com>
2026-02-09 14:24:25 -0500

    Update epistorm-ensemble-dashboard.py

--- epistorm-ensemble-dashboard.py
+++ epistorm-ensemble-dashboard.py
@@ -1403,7 +1403,7 @@ with tab_overview:
                             y0=lower, y1=upper,
                             fillcolor=ACTIVITY_COLORS[level],
                             line_width=0,
-                            opacity=0.3,
+                            opacity=0.9,
                            ) 
 
                     fig.update_layout(yaxis=dict(range=[0, y_max]))
