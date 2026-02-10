390586ec18e8390ca7eaf0b99c6b07d9e1d23fbb
clarabay <98752502+clarabay@users.noreply.github.com>
2026-02-10 12:41:08 -0500

    Update epistorm-ensemble-dashboard.py

--- epistorm-ensemble-dashboard.py
+++ epistorm-ensemble-dashboard.py
@@ -1503,7 +1503,7 @@ with tab_overview:
 
 
     with row2_col2:
-        with st.container(border=True, height=400):
+        with st.container(border=True, height=500):
             
             cat_df = pd.read_parquet('./data/categorical_ensemble.pq')
             cat_df['reference_date'] = pd.to_datetime(cat_df['reference_date'])
