6e238e6efbb2d43a792f319fa64c8a4505a84406
clarabay <98752502+clarabay@users.noreply.github.com>
2026-02-10 11:50:21 -0500

    Update epistorm-ensemble-dashboard.py

--- epistorm-ensemble-dashboard.py
+++ epistorm-ensemble-dashboard.py
@@ -1403,7 +1403,7 @@ with tab_overview:
                             y0=lower, y1=upper,
                             fillcolor=ACTIVITY_COLORS[level],
                             line_width=0,
-                            opacity=0.6,
+                            opacity=0.7,
                             layer="below"
                            ) 
 
