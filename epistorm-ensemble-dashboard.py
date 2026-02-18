53f5b0228c601969130659f6ddd2b44ca4d2a76b
clarabay <98752502+clarabay@users.noreply.github.com>
2026-02-18 16:11:54 -0500

    Update epistorm-ensemble-dashboard.py

--- epistorm-ensemble-dashboard.py
+++ epistorm-ensemble-dashboard.py
@@ -79,7 +79,7 @@ QUANTILES = ['0.01', '0.025', '0.05', '0.1', '0.15', '0.2', '0.25', '0.3', '0.35
              '0.85', '0.9', '0.95', '0.975', '0.99']
 
 COLOR_MAP = {
-    'Median Epistorm Ensemble': '#1f77b4',
+    'Median Epistorm Ensemble': 'seagreen',
     'MIGHTE-Nsemble': '#2ca02c',
     'MIGHTE-Joint': '#d62728',
     'CEPH-Rtrend_fluH': '#9467bd',
